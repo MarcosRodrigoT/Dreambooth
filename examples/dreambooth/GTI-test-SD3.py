b99fcb42a425e1eb6363f272f10db3e0b1522722
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2025-02-10 15:06:00 +0100

    Add negative prompt during inference

--- examples/dreambooth/GTI-test-SD3.py
+++ examples/dreambooth/GTI-test-SD3.py
@@ -5,32 +5,36 @@ from diffusers import StableDiffusion3Pipeline
 
 base_model = "stabilityai/stable-diffusion-3-medium-diffusers"
 
-person = "mrt"
-fine_tuned_model = f"{person}-model"
-save_to = f"{person}-picture.png"
-prompt = f"a photo of {person} as a clown"
+PERSON = "mrt"
+FINE_TUNED_MODEL = f"{PERSON}-model"
+SAVE_TO = f"{PERSON}-picture.png"
+PROMPT = f"a photo of {PERSON} as a clown"
+NEGATIVE_PROMPT = "blurry, low quality, low resolution"
 
 # Load base model
 pipe = StableDiffusion3Pipeline.from_pretrained(
     base_model,
     # num_inference_steps=20,
-    guidance_scale=2.0,
+    guidance_scale=5.0,
     height=1024,
     width=1024,
     torch_dtype=torch.float16,
 ).to("cuda")
 
 # Create directory for images
-image_dir = f"{person}-images"
+image_dir = f"{PERSON}-images"
 os.makedirs(image_dir, exist_ok=True)
 
 # Load LoRA weights
-pipe.load_lora_weights(fine_tuned_model)
+pipe.load_lora_weights(FINE_TUNED_MODEL)
 
 # Generate the image
-image = pipe(prompt).images[0]
+image = pipe(
+    PROMPT,
+    negative_prompt=NEGATIVE_PROMPT,
+).images[0]
 
 # Save image with timestamp
 timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-save_to = os.path.join(image_dir, f"{timestamp}.png")
-image.save(save_to)
+SAVE_TO = os.path.join(image_dir, f"{timestamp}.png")
+image.save(SAVE_TO)
