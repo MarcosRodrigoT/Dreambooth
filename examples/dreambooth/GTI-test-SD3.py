ac3187ac5812c9e317282bfbdef430bfb6667af8
MarcosRodrigoT <marcosrodrigo5@hotmail.com>
2025-02-10 14:25:02 +0100

    Save images generated for each user in an orderly manner

--- examples/dreambooth/GTI-test-SD3.py
+++ examples/dreambooth/GTI-test-SD3.py
@@ -1,13 +1,14 @@
+import os
 import torch
+from datetime import datetime
 from diffusers import StableDiffusion3Pipeline
-
 
 base_model = "stabilityai/stable-diffusion-3-medium-diffusers"
 
 person = "mrt"
 fine_tuned_model = f"{person}-model"
 save_to = f"{person}-picture.png"
-prompt = f"a photo of {person} as an evil debtor"
+prompt = f"a photo of {person} as a clown"
 
 # Load base model
 pipe = StableDiffusion3Pipeline.from_pretrained(
@@ -19,8 +20,17 @@ pipe = StableDiffusion3Pipeline.from_pretrained(
     torch_dtype=torch.float16,
 ).to("cuda")
 
+# Create directory for images
+image_dir = f"{person}-images"
+os.makedirs(image_dir, exist_ok=True)
+
 # Load LoRA weights
 pipe.load_lora_weights(fine_tuned_model)
 
+# Generate the image
 image = pipe(prompt).images[0]
+
+# Save image with timestamp
+timestamp = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+save_to = os.path.join(image_dir, f"{timestamp}.png")
 image.save(save_to)
